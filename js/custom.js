445357ae92cfa0ffec592cd6fd7af849bacad79d
Akaihane <97947281+Akaihane@users.noreply.github.com>
2022-04-13 12:15:23 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.typing', {
+    strings: ["Graphic Designer.", "Web Designer.", "Code Developer."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+new WOW().init();
